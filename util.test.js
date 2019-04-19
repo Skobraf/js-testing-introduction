2d53832d86c7a6efab6c6d8f1ef04c2107413a26
skobraf <skobraf@gmail.com>
2019-04-19 10:49:11 +0100

    integration test

--- util.test.js
+++ util.test.js
@@ -1,5 +1,5 @@
-const { generateText } = require('./util');
-
+const { generateText, checkAndGenerate } = require('./util');
+// UNIT TEST
 test('should output name and age', () => {
     const text = generateText('Ayoub', 25);
     expect(text).toBe('Ayoub (25 years old)');
@@ -12,4 +12,9 @@ test('should output nothing', () => {
     expect(text).toBe(' (null years old)');
     const text2 =  generateText();
     expect(text2).toBe('undefined (undefined years old)');
+})
+// INTEGRETED TEST
+test('should output name and age', () => {
+    const text = checkAndGenerate('ayoub', 25);
+    expect(text).toBe('ayoub (25 years old)');
 })
